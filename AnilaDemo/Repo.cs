3f833ae2827bf84581cbf2559bbf78d06907572e
Ranjit Ramachandran Pillai <ranjit.pillai@testhouse.net>
2019-06-13 10:44:54 +0530

    Add project files.

---
+++ AnilaDemo/Repo.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnilaDemo
+{
+    public class Repo
+    {
+
+        public static By GoogleSearchField = By.XPath("//*[@id='tsf']/div[2]/div/div[1]/div/div[1]/input");
+        public static By GoogleSearchBtn = By.XPath("//*[@id='tsf']/div[2]/div/div[3]/center/input[1]");
+
+    }
+}
